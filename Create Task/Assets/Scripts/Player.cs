0cb72989fae7723f2b8605addf946d58e0bf7ff5
Koben <kobenmcclellan@gmail.com>
2020-01-11 21:42:27 -0800

    Jumpy doin a fucky wucky

--- Create Task/Assets/Scripts/Player.cs
+++ Create Task/Assets/Scripts/Player.cs
@@ -6,13 +6,18 @@ public class Player : MonoBehaviour
 {
     SpriteRenderer mySr;
     Animator myAnim;
+    Rigidbody2D myRb;
     public GameObject player;
     public float Speed = 5f;
+    public Vector2 jumpHeight;
+    public float jump;
+    private bool isJumping = false;
     // Start is called before the first frame update
     void Start()
     {
         mySr = GetComponent<SpriteRenderer>();
         myAnim = GetComponent<Animator>();
+        myRb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
@@ -45,6 +50,19 @@ public class Player : MonoBehaviour
     }
     void jumping()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            myRb.AddForce(Vector2.up * jumpHeight);
+            isJumping = true;
 
+        }
     }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            isJumping = false;
+        }
+    }
+
 }
