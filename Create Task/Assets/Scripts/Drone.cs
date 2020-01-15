b6488a29e6a17127372e080d576b556d0f038af8
Koben <kobenmcclellan@gmail.com>
2020-01-14 17:56:28 -0800

    Made drone following code and added to player script to die on thorns and switch scenes when dying to death screen

---
+++ Create Task/Assets/Scripts/Drone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    public float speed = 4.5f;
+    public Transform target;
+    private bool checkTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+    }
+}
