81d4ff9a78987c83da1af1da896d8fe92414d392
Jordanluma <jordan.luma@digipen.edu>
2020-01-15 11:22:26 -0800

    scenes

---
+++ Create Task/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToDeathScene()
+    {
+        SceneManager.LoadScene("Death");
+    }
+
+    public void GoToGameStart()
+    {
+        SceneManager.LoadScene("DesignScene");
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
