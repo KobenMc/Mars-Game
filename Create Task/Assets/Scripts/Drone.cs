bd18d3787948ec1f790cf756131c11e8fc43f940
Koben <kobenmcclellan@gmail.com>
2020-01-16 14:04:04 -0800

    Tested random things

--- Create Task/Assets/Scripts/Drone.cs
+++ Create Task/Assets/Scripts/Drone.cs
@@ -7,7 +7,8 @@ public class Drone : MonoBehaviour
     public float speed = 4.5f;
     public Transform target;
     private bool checkTrigger;
-
+    SpriteRenderer mySr;
+    Animator myAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +26,6 @@ public class Drone : MonoBehaviour
     {
         //Go to player only on x axis
         transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
+
     }
 }
