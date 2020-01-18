7078a4b9410e33a9628d02630fdf44d42db5a8bf
Koben McClellan <koben.mcclellan@digipen.edu>
2020-01-17 18:03:30 -0800

    Made damage for enemies and death

--- Create Task/Assets/Scripts/Drone.cs
+++ Create Task/Assets/Scripts/Drone.cs
@@ -7,14 +7,18 @@ public class Drone : MonoBehaviour
     public float speed = 4.5f;
     public Transform target;
     private bool checkTrigger;
+    int health = 75;
+    Rigidbody2D myRb;
     SpriteRenderer mySr;
     Animator myAnim;
+    public GameObject self;
     // Start is called before the first frame update
     void Start()
     {
         //Find player
         mySr = GetComponent<SpriteRenderer>();
         myAnim = GetComponent<Animator>();
+        myRb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
     }
 
@@ -22,6 +26,7 @@ public class Drone : MonoBehaviour
     void Update()
     {
         Movement();
+        Death();
     }
 
     void Movement()
@@ -32,6 +37,21 @@ public class Drone : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
             myAnim.SetInteger("drone", 2);
+        }
+    }
+    void Death()
+    {
+        if (health <= 0)
+        {
+            myAnim.SetInteger("drone", 3);
+            Object.Destroy(self, 1);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "sword")
+        {
+            health -= 25;
         }
     }
 }
